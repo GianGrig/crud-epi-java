7fc2bb7ed314ea8fafa6de65daf2e094f5b61aed
GIANLUCA GRIGNANI <gianluca_grignani@estudante.sesisenai.org.br>
2025-04-03 19:13:05 -0300

    Criação da classe Menu

--- src/SistemaEPIs/Menu/Menu.java
+++ src/SistemaEPIs/Menu/Menu.java
@@ -1,4 +1,32 @@
 package SistemaEPIs.Menu;
 
+import java.util.Scanner;
+
 public class Menu {
+
+    public static void  main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\n--- Menu Principal ---");
+            System.out.println("1. Gerenciar Usuários");
+            System.out.println("2. Gerenciar EPIs");
+            System.out.println("3. Gerenciar Empréstimos");
+            System.out.println("4. Gerenciar Devoluções");
+            System.out.println("0. Sair");
+            System.out.println("Escolha: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1 -> menuUsuario();
+                case 2 -> menuEpi();
+                case 3 -> menuEmprestimos();
+                case 4 -> menuDevolucao();
+                case 0 -> System.out.println("Saindo.");
+                default -> System.out.println("Opção inválida.");
+            }
+        } while (opcao != 0);
+    }
 }
